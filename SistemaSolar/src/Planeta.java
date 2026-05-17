1fe128897780f5c30dcf9da388b219877d6fc661
gonzalezfrutosmateo-cyber <gonzalezfrutosmateo@gmail.com>
2026-05-17 10:56:20 -0300

    ejercicio del sistema solar terminado

--- SistemaSolar/src/Planeta.java
+++ SistemaSolar/src/Planeta.java
@@ -1,70 +1,98 @@
-
 import java.util.Scanner;
 
-
 public class Planeta {
-    
+
     String nombre;
     int cantDeSatelites;
     double masaKg;
     double volumenKmCubicos;
     int diametroKm = 0;
-    int distanciaMediaAlSol = 0;
+    long distanciaMediaAlSol = 0; 
     String tipoDePlaneta;
     boolean observable = false;
-    
-    public Planeta(String nombre,int cantDeSatelites,double masaKg,double volumenKmCubicos, int diametroKm , int distanciaMediaAlSol, String tipoDePlaneta, boolean observable){
-    this.nombre = nombre;
-    this.cantDeSatelites = cantDeSatelites;
-    this.masaKg = masaKg;
-    this.volumenKmCubicos = volumenKmCubicos;
-    this.diametroKm = diametroKm;
-    this.distanciaMediaAlSol = distanciaMediaAlSol;
-    this.tipoDePlaneta = tipoDePlaneta;
-    this.observable = observable;
-    }
-    
+    double periodoOrbital;
+    double periodoDeRotacion;
+
+    public Planeta(String nombre, int cantDeSatelites, double masaKg, double volumenKmCubicos, int diametroKm, long distanciaMediaAlSol, String tipoDePlaneta, boolean observable, double periodoOrbital, double periodoDeRotacion) {
+        this.nombre = nombre;
+        this.cantDeSatelites = cantDeSatelites;
+        this.masaKg = masaKg;
+        this.volumenKmCubicos = volumenKmCubicos;
+        this.diametroKm = diametroKm;
+        this.distanciaMediaAlSol = distanciaMediaAlSol;
+        this.tipoDePlaneta = tipoDePlaneta;
+        this.observable = observable;
+        this.periodoOrbital = periodoOrbital;
+        this.periodoDeRotacion = periodoDeRotacion;
         
-    
-    public void menuPlaneta(){
-    Scanner sca = new Scanner(System.in);
-    System.out.println("-------------------------------------------------------------------------------");
-    System.out.println(nombre);
-    System.out.println("Datos:");
-    System.out.println("1- Cantidad de satelites");
-    System.out.println("2- Masa");
-    System.out.println("3- Volumen");
-    System.out.println("4- Diametro");
-    System.out.println("5- Distancia media al sol");
-    System.out.println("6- Tipo de planeta");
-    System.out.println("7- Observable");
-    System.out.println("8- Densidad");
-    System.out.println("9- Es un planeta exterior?");
-    System.out.println("-------------------------------------------------------------------------------");
-    int opcion = sca.nextInt();
-    switch (opcion) {
-                case 1: System.out.println("La cantidad de satelites de" + nombre + " es:" + cantDeSatelites);
-                case 2: System.out.println("La Masa en kilogramos de" + nombre + " es:" + masaKg);
-                case 3: System.out.println("El volumen en kilometros cubicos es de" + nombre + " es:" + volumenKmCubicos);
-                case 4: System.out.println("El diametro en kilometros es de" + nombre + " es:" + diametroKm);
-                case 5: System.out.println("La distancia media al sol de" + nombre + " es:" + distanciaMediaAlSol);
-                case 6: System.out.println(nombre + " es un planeta" + tipoDePlaneta);
-                case 7: System.out.println(nombre + " es:" + observable + "a plena vista!");
-                case 8: System.out.println("La densidad de" + nombre + " es:" + .calcularDensidad);
-                case 9: System.out.println("La cantidad de satelites de" + nombre + " es:" + .planetaExterior);
-                default: System.out.println("Ingrese una opcion valida");
     }
-    
-    public int calcularDensidad(){
-        return masaKg/volumenKmCubicos;
+
+    public void menuPlaneta() {
+        Scanner sca = new Scanner(System.in);
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println(nombre);
+        System.out.println("Datos:");
+        System.out.println("1- Cantidad de satelites");
+        System.out.println("2- Masa");
+        System.out.println("3- Volumen");
+        System.out.println("4- Diametro");
+        System.out.println("5- Distancia media al sol");
+        System.out.println("6- Tipo de planeta");
+        System.out.println("7- Observable");
+        System.out.println("8- Densidad");
+        System.out.println("9- Es un planeta exterior?");
+        System.out.println("10- Periodo orbital (en años)");
+        System.out.println("11- Periodo de rotacion (en dias");
+        System.out.println("-------------------------------------------------------------------------------");
+        int opcion = sca.nextInt();
+        switch (opcion) {
+            case 1:
+                System.out.println("La cantidad de satelites de " + nombre + " es: " + cantDeSatelites);
+                break;
+            case 2:
+                System.out.println("La masa en kilogramos de " + nombre + " es: " + masaKg + " kg");
+                break;
+            case 3:
+                System.out.println("El volumen en kilometros cubicos de " + nombre + " es: " + volumenKmCubicos + " km3");
+                break;
+            case 4:
+                System.out.println("El diametro en kilometros de " + nombre + " es: " + diametroKm + " km");
+                break;
+            case 5:
+                System.out.println("La distancia media al sol de " + nombre + " es: " + distanciaMediaAlSol + " km");
+                break;
+            case 6:
+                System.out.println(nombre + " es un planeta " + tipoDePlaneta);
+                break;
+            case 7:
+                System.out.println(nombre + " es observable a simple vista: " + observable);
+                break;
+            case 8:
+                System.out.println("La densidad de " + nombre + " es: " + calcularDensidad() + " kg/km3");
+                break;
+            case 9:
+                System.out.println(nombre + " es un planeta exterior: " + planetaExterior());
+                break;
+            default:
+                System.out.println("Ingrese una opcion valida");
+            case 10:
+                System.out.println("El periodo orbital de " + nombre + " es de: " + periodoOrbital + " años");
+                break;
+            case 11:
+                System.out.println("La distancia media al sol de " + nombre + " es: " + periodoDeRotacion + " dias");
+                break;
+        }
     }
-    
-    publid void planetaExterior(){
-        if (distanciaMediaAlSol>=508632760){
-        return true;
+
+    public double calcularDensidad() {
+        return masaKg / volumenKmCubicos;
+    }
+
+    public boolean planetaExterior() {
+        if (distanciaMediaAlSol >= 508632760) {
+            return true;
+        } else {
+            return false;
         }
-        else{
-        return false;
-        }
-    } 
+    }
 }
--- SistemaSolar/src/SistemaSolar.java
+++ SistemaSolar/src/SistemaSolar.java
@@ -5,48 +5,49 @@ public class SistemaSolar {
         Scanner sca = new Scanner(System.in);
         int continuar = 0;
         
-        Planeta mercurio = new Planeta ("Mercurio" , 0, 3.3010000000000000000000000, 6.0830000000, 4881, 57000000, "Rocoso", true);
-        Planeta venus = new Planeta ("Venus" , 0, 4.869000000000000000000000, 9.38000000000, 12103, 108000000, "Rocoso", true);
-        Planeta tierra = new Planeta ("Tierra" , 1, 5.97200000000000000000000, 1.000000000000, 12756, 147000000, "Rocoso", true);
-        Planeta marte = new Planeta ("Marte" , 2, 6.41910000000000000000000, 1.63000000000, 6779, 227940000, "Rocoso", true);
-        Planeta jupiter = new Planeta ("Jupiter" , 1, 3.3010000000000000000000000, 6.0830000000, 4881, 57, "Rocoso", true);
-        Planeta saturno = new Planeta ("Saturno" , 1, 3.3010000000000000000000000, 6.0830000000, 4881, 57, "Rocoso", true);
-        Planeta urano = new Planeta ("Urano" , 1, 3.3010000000000000000000000, 6.0830000000, 4881, 57, "Rocoso", true);
-        Planeta neptuno = new Planeta ("Neptuno" , 1, 3.3010000000000000000000000, 6.0830000000, 4881, 57, "Rocoso", true);
-        Planeta pluton = new Planeta ("Pluton" , 1, 3.3010000000000000000000000, 6.0830000000, 4881, 57, "Rocoso", true);
-        do {
-        System.out.println("-------------------------------------------------------------------------------");
-        System.out.println("Eliga el planeta que desea conocer!");
-        System.out.println("Planetas:");
-        System.out.println("1- Mercurio");
-        System.out.println("2- Venus");
-        System.out.println("3- Tierra");
-        System.out.println("4- Marte");
-        System.out.println("5- Jupiter");
-        System.out.println("6- Saturno");
-        System.out.println("7- Urano");
-        System.out.println("8- Neptuno");
-        System.out.println("9- Pluton");
-        System.out.println("-------------------------------------------------------------------------------");
-    
-        int opcion = sca.nextInt();
+        Planeta mercurio = new Planeta("Mercurio", 0,  3.301e23,  6.083e10,   4879,   57910000,  "Rocoso",  true , 0.24 , 58.65);
+        Planeta venus    = new Planeta("Venus",    0,  4.867e24,  9.28e11,   12104,  108200000,  "Rocoso",  true , 0.62 , 243.02);
+        Planeta tierra   = new Planeta("Tierra",   1,  5.972e24,  1.083e12,  12756,  149600000,  "Rocoso",  true , 1 , 1);
+        Planeta marte    = new Planeta("Marte",    2,  6.417e23,  1.631e11,   6779,  227940000,  "Rocoso",  true , 1.88 , 1.03);
+        Planeta jupiter  = new Planeta("Jupiter",  95, 1.898e27,  1.431e15,  142984,  778500000,  "Gaseoso", true , 11.86 , 0.41);
+        Planeta saturno  = new Planeta("Saturno",  83, 5.683e26,  8.271e14,  120536, 1432000000,  "Gaseoso", true , 29.46 , 0.44);
+        Planeta urano    = new Planeta("Urano",    27, 8.681e25,  6.833e13,   51118, 2872500000L,  "Gaseoso", true , 84.01 , 0.72);
+        Planeta neptuno  = new Planeta("Neptuno",  16, 1.024e26,  6.254e13,   49528, 4495100000L,  "Gaseoso", false , 164.8 , 0.67);
+        Planeta pluton   = new Planeta("Pluton",    5, 1.309e22,  6.387e9,     2376, 5906400000L,  "Rocoso",  false , 258.09 , 6.39);
 
+        do {
+            System.out.println("-------------------------------------------------------------------------------");
+            System.out.println("Elija el planeta que desea conocer!");
+            System.out.println("Planetas:");
+            System.out.println("1- Mercurio");
+            System.out.println("2- Venus");
+            System.out.println("3- Tierra");
+            System.out.println("4- Marte");
+            System.out.println("5- Jupiter");
+            System.out.println("6- Saturno");
+            System.out.println("7- Urano");
+            System.out.println("8- Neptuno");
+            System.out.println("9- Pluton");
+            System.out.println("-------------------------------------------------------------------------------");
+
+            int opcion = sca.nextInt();
             switch (opcion) {
-                case 1: mercurio.MenuPlaneta();
-                case 2: venus.MenuPlaneta();
-                case 3: tierra.MenuPlaneta();
-                case 4: marte.MenuPlaneta();
-                case 5: jupiter.MenuPlaneta();
-                case 6: saturno.MenuPlaneta();
-                case 7: urano.MenuPlaneta();
-                case 8: neptuno.MenuPlaneta();
-                case 9: pluton.MenuPlaneta();
+                case 1: mercurio.menuPlaneta(); break;
+                case 2: venus.menuPlaneta();    break;
+                case 3: tierra.menuPlaneta();   break;
+                case 4: marte.menuPlaneta();    break;
+                case 5: jupiter.menuPlaneta();  break;
+                case 6: saturno.menuPlaneta();  break;
+                case 7: urano.menuPlaneta();    break;
+                case 8: neptuno.menuPlaneta();  break;
+                case 9: pluton.menuPlaneta();   break;
                 default: System.out.println("Ingrese una opcion valida");
-            } 
-            
-                }
-        System.out.println("Desea continuar 1-si 2-no");
-            continuar = sca.nextInt();    
-        }    
-    while (continuar == 1);
+            }
+
+            System.out.println("Desea continuar? 1-Si  2-No");
+            continuar = sca.nextInt();
+
+        } while (continuar == 1);
+        
+    }
 }
